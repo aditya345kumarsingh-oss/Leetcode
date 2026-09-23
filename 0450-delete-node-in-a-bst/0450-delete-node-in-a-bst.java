86cfc973b757144e1dcef02cdbd0cbf02f322ced
Aditya Kumar Singh <aditya345kumarsingh@gmail.com>
2026-09-23 11:30:44 +0530

    Time: 0 ms (100%), Space: 47.7 MB (5.32%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        // Search for the target node
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            // Target node found: handle deletion cases
+
+            // Case 1 & 2: Node has 0 or 1 child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // Case 3: Node has two children
+            // Find the inorder successor (minimum node in the right subtree)
+            TreeNode successor = findMin(root.right);
+            root.val = successor.val;
+
+            // Delete the successor node from the right subtree
+            root.right = deleteNode(root.right, successor.val);
+        }
+
+        return root;
+    }
+
+    private TreeNode findMin(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+}
